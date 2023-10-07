e840a56801532d0da02cf6f4329ab441258c9ce0
kdybe01 <kdybe01@gmail.com>
2023-10-07 23:11:27 +0900

    ✨feat : 영수증 조회 로직 추가

---
+++ api/src/main/java/mutsa/api/service/payment/ReceiptService.java
@@ -0,0 +1,24 @@
+package mutsa.api.service.payment;
+
+import lombok.RequiredArgsConstructor;
+import mutsa.api.dto.payment.ReceiptCardResponseDto;
+import mutsa.api.dto.payment.ReceiptResponseDto;
+import mutsa.common.domain.models.payment.CardReceipt;
+import mutsa.common.domain.models.payment.Receipt;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReceiptService {
+    private final ReceiptModuleService receiptModuleService;
+    private final CardReceiptService cardReceiptService;
+
+    public ReceiptResponseDto getReceipt(String receiptApiId) {
+        Receipt receipt = receiptModuleService.getReceiptByApiId(receiptApiId);
+        CardReceipt cardReceipt = cardReceiptService.getByReceipt(receipt);
+        ReceiptCardResponseDto receiptCardDto = ReceiptCardResponseDto.of(cardReceipt);
+        ReceiptResponseDto receiptDto = ReceiptResponseDto.of(receipt);
+        receiptDto.setCard(receiptCardDto);
+        return receiptDto;
+    }
+}
--- common/src/main/java/mutsa/common/repository/payment/ReceiptRepository.java
+++ common/src/main/java/mutsa/common/repository/payment/ReceiptRepository.java
@@ -3,5 +3,8 @@ package mutsa.common.repository.payment;
 import mutsa.common.domain.models.payment.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+    Optional<Receipt> findByApiId(String apiId);
 }
