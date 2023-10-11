6e45b1ebd3905759f2d7617248ec97cf8e2a148e
mon0mon <mon0mon@naver.com>
2023-10-11 17:14:46 +0900

    ♻️refactor: ImageModuleService - saveAll 메소드 수정 (ImageReference 파라미터 추가) - saveAllReviewImage 메소드 주석 - deleteAllByRefId 메소드에서 작성자인지 확인하는 로직 주석

--- api/src/main/java/mutsa/api/service/image/ImageModuleService.java
+++ api/src/main/java/mutsa/api/service/image/ImageModuleService.java
@@ -29,7 +29,7 @@ public class ImageModuleService {
     private final UserRepository userRepository;
 
     @Transactional
-    public List<Image> saveAll(List<ImagesRequestDto> imagesRequestDtos, String articleApiId) {
+    public List<Image> saveAll(List<ImagesRequestDto> imagesRequestDtos, String refApiId, ImageReference imgRefType) {
         User currentUser = userRepository
                 .findByUsername(SecurityUtil.getCurrentUsername())
                 .orElseThrow(() -> new BusinessException(ErrorCode.SECURITY_CONTEXT_ERROR));
@@ -43,8 +43,8 @@ public class ImageModuleService {
                             .fileName(imagesRequestDtos.get(i).getFilename())
                             .user(currentUser)
                             .imgIdx(i)
-                            .imageReference(ImageReference.ARTICLE)
-                            .refApiId(articleApiId)
+                            .imageReference(imgRefType)
+                            .refApiId(refApiId)
                             .status(Status.ACTIVE)
                             .build()
             );
@@ -53,35 +53,41 @@ public class ImageModuleService {
         images = imageRepository.saveAll(images);
         return images;
     }
-    @Transactional
-    public List<Image> saveAllReviewImage(List<ImagesRequestDto> imagesRequestDtos, String reviewApiId) {
-        User currentUser = userRepository
-            .findByUsername(SecurityUtil.getCurrentUsername())
-            .orElseThrow(() -> new BusinessException(ErrorCode.SECURITY_CONTEXT_ERROR));
-
-        List<Image> images = new ArrayList<>();
-
-        for (int i = 0; i < imagesRequestDtos.size(); i++) {
-            images.add(
-                Image.builder()
-                    .path(imagesRequestDtos.get(i).getS3URL())
-                    .fileName(imagesRequestDtos.get(i).getFilename())
-                    .user(currentUser)
-                    .imgIdx(i)
-                    .imageReference(ImageReference.REVIEW)
-                    .refApiId(reviewApiId)
-                    .status(Status.ACTIVE)
-                    .build()
-            );
-        }
-
-        images = imageRepository.saveAll(images);
-        return images;
-    }
+//    @Transactional
+//    public List<Image> saveAllReviewImage(List<ImagesRequestDto> imagesRequestDtos, String reviewApiId) {
+//        User currentUser = userRepository
+//            .findByUsername(SecurityUtil.getCurrentUsername())
+//            .orElseThrow(() -> new BusinessException(ErrorCode.SECURITY_CONTEXT_ERROR));
+//
+//        List<Image> images = new ArrayList<>();
+//
+//        for (int i = 0; i < imagesRequestDtos.size(); i++) {
+//            images.add(
+//                Image.builder()
+//                    .path(imagesRequestDtos.get(i).getS3URL())
+//                    .fileName(imagesRequestDtos.get(i).getFilename())
+//                    .user(currentUser)
+//                    .imgIdx(i)
+//                    .imageReference(ImageReference.REVIEW)
+//                    .refApiId(reviewApiId)
+//                    .status(Status.ACTIVE)
+//                    .build()
+//            );
+//        }
+//
+//        images = imageRepository.saveAll(images);
+//        return images;
+//    }
 
 
     @Transactional
     public void deleteByRefApiId(String refApiId) {
+        //  REFACTOR 유저 로그인 하는 부분 AOP로 처리해보는 방법 고민하기
+        //  REFACTOR 어드민 권한이 있는 유저의 경우도 예외처리하는 AOP 고민하기
+//        User currentUser = userRepository
+//                .findByUsername(SecurityUtil.getCurrentUsername())
+//                .orElseThrow(() -> new BusinessException(ErrorCode.SECURITY_CONTEXT_ERROR));
+
         List<Image> images = imageRepository.getAllByRefApiId(refApiId);
 
         images.forEach(image -> image.setStatus(Status.DELETED));
@@ -89,18 +95,25 @@ public class ImageModuleService {
 
     @Transactional
     public void deleteAllByRefId(String refApiId) {
-        User currentUser = userRepository
-                .findByUsername(SecurityUtil.getCurrentUsername())
-                .orElseThrow(() -> new BusinessException(ErrorCode.SECURITY_CONTEXT_ERROR));
+        //  REFACTOR 유저 로그인 하는 부분 AOP로 처리해보는 방법 고민하기
+        //  REFACTOR 어드민 권한이 있는 유저의 경우도 예외처리하는 AOP 고민하기
+//        User currentUser = userRepository
+//                .findByUsername(SecurityUtil.getCurrentUsername())
+//                .orElseThrow(() -> new BusinessException(ErrorCode.SECURITY_CONTEXT_ERROR));
 
         List<Image> images = imageRepository.getAllByRefApiId(refApiId);
 
         images.forEach(image -> {
-            if (!image.getUser().equals(currentUser)) {
-                throw new BusinessException(ErrorCode.IMAGE_USER_NOT_MATCH);
-            }
+            //  일시적으로 유저가 작성한 이미지인지 확인하는 기능 주석
+//            if (!image.getUser().equals(currentUser)) {
+//                throw new BusinessException(ErrorCode.IMAGE_USER_NOT_MATCH);
+//            }
 
             image.setStatus(Status.DELETED);
         });
+    }
+
+    public List<Image> getAllByRefId(String refApiId) {
+        return imageRepository.getAllByRefApiId(refApiId);
     }
 }
