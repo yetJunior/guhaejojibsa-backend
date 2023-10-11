2e3b9fa1408941a4bc765648d40379bffdc72fff
mon0mon <mon0mon@naver.com>
2023-10-11 17:11:45 +0900

    ✨feat : ImageService 추가

---
+++ api/src/main/java/mutsa/api/service/image/ImageService.java
@@ -0,0 +1,51 @@
+/**
+ * @project backend
+ * @author ARA
+ * @since 2023-10-11 PM 4:50
+ */
+
+package mutsa.api.service.image;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import mutsa.api.dto.image.ImageResponseDto;
+import mutsa.api.dto.image.ImagesRequestDto;
+import mutsa.common.domain.models.image.Image;
+import mutsa.common.domain.models.image.ImageReference;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+    private final ImageModuleService imageModuleService;
+
+    @Transactional
+    public List<ImageResponseDto> saveAll(List<ImagesRequestDto> imagesRequestDtos, String articleApiId, ImageReference imgRefType) {
+        return imageModuleService.saveAll(imagesRequestDtos, articleApiId, imgRefType)
+                .stream()
+                .sorted(Comparator.comparing(Image::getId))
+                .map(image -> ImageResponseDto.to(image))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void deleteByRefApiId(String refApiId) {
+        imageModuleService.deleteByRefApiId(refApiId);
+    }
+
+    @Transactional
+    public void deleteAllByRefId(String refApiId) {
+        imageModuleService.deleteAllByRefId(refApiId);
+    }
+
+    public List<ImageResponseDto> getAllByRefId(String refApiId) {
+        return imageModuleService.getAllByRefId(refApiId)
+                .stream()
+                .sorted(Comparator.comparing(Image::getId))
+                .map(image -> ImageResponseDto.to(image))
+                .collect(Collectors.toList());
+    }
+}
