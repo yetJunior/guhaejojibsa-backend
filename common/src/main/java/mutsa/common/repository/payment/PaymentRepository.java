17c81b1cc4b621da21d950a97b11c9704c21f34a
kdybe01 <kdybe01@gmail.com>
2023-10-13 14:19:36 +0900

    ♻️refactor : 주문으로 결제 정보를 찾을 수 있는 메소드 추가

--- common/src/main/java/mutsa/common/repository/payment/PaymentRepository.java
+++ common/src/main/java/mutsa/common/repository/payment/PaymentRepository.java
@@ -1,5 +1,6 @@
 package mutsa.common.repository.payment;
 
+import mutsa.common.domain.models.order.Order;
 import mutsa.common.domain.models.payment.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,5 @@ import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByOrderKey(String orderKey);
+    Optional<Payment> findByOrder(Order order);
 }
